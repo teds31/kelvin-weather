d81e179a3a2519474abf792fcdadb22a15c10ce2
TEDDY STANOWSKI <TSTANOWSKI31@GMAIL.COM>
2017-09-30 12:14:21 -0700

    add prompt for users to enter kelvin temperature

--- temperature.js
+++ temperature.js
@@ -1,14 +1,12 @@
 //kelvin variable that is constant
-const kelvin = 294
+//add a prompt for a user to enter a question
+const kelvin = prompt('What is the Kelvin temperatue today?')
 
 //celcius variable which is 273 minus kelvin amount
 const celsius = (kelvin - 273);
 
 //calculate fahrenheit with celsius and round down
 const fahrenheit = Math.floor(celsius * (9/5) + 32);
-
-//prompt a question for a user to enter the temperature in kelvin
-prompt
 
 //print to console what the temparture is of 294 kelvin in fahrenheit
 console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`)
